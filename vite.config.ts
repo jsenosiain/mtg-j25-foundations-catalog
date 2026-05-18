570bd6711bac7165700f2ae6a8ff12882c8f375b
Joaquin Senosiain <joaco182@yahoo.com>
2026-05-18 09:26:17 -0700

    Github Pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mtg-j25-foundations-catalog/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
